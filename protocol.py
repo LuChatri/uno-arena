b754c81677462fbfe11854359d46929d56f328f6
CluelessMe <ldcmiga@gmail.com>
2020-11-23 13:30:45 -0700

    Make protocol.py

---
+++ protocol.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''This module provides constants for Universal Uno Interface communication'''
+
+BOOTING = 'booting\n'
+ERROR = 'error {message}\n'
+GAMESTATE = 'gamestate {game_info}\n'
+GO = 'go\n'
+MOVE = 'move {move_info}\n'
+READY = 'ready\n'
+SHUTDOWN = 'shutdown\n'
+SHUTTINGDOWN = 'shuttingdown\n'
+STOP = 'stop\n'
