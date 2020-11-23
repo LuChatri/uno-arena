9611b6d49de79e1c6394e775772101a02e5c2cf2
CluelessMe <ldcmiga@gmail.com>
2020-11-23 14:15:14 -0700

    Change stuff

--- protocol.py
+++ protocol.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-'''This module provides constants for Universal Uno Interface communication'''
+'''This module provides constants and parsing utility for Universal Uno Interface communication'''
 
-BOOTING = 'booting\n'
-ERROR = 'error {message}\n'
-GAMESTATE = 'gamestate {game_info}\n'
-GO = 'go\n'
-MOVE = 'move {move_info}\n'
-READY = 'ready\n'
-SHUTDOWN = 'shutdown\n'
-SHUTTINGDOWN = 'shuttingdown\n'
-STOP = 'stop\n'
+BOOTING = 'booting'
+ERROR = 'error {message}'
+GAMESTATE = 'gamestate {game_info}'
+GO = 'go'
+MOVE = 'move {move_info}'
+READY = 'ready'
+SHUTDOWN = 'shutdown'
+SHUTTINGDOWN = 'shuttingdown'
+STOP = 'stop'
+
+SHUTDOWN_TIMEOUT = 30
